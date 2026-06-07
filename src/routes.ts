60787dd4cd09ea6c16868f385809567593e8c0ff
Harsh Thakur <harshprojects2002@outlook.com>
2026-06-08 00:00:50 +0530

    refactor: rename folder `middleware` to `middlewares`

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { requestLogger } from "#middleware/request-logger.js";
+import { requestLogger } from "#lib/middlewares/request-logger.js";
 import healthRouter from "#routes/health.js";
 import greetRouter from "#routes/greet.js";
 
